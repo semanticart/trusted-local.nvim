e480e9c027888c1ceb70d03b09528c3501fe1c18
Jeffrey Chupp <jeff@semanticart.com>
2023-04-13 09:37:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This plugin will load `.init.local.lua` from the root of a git repo IF you've marked it as safe.
+
+You can mark a repo as safe by doing a `mkdir .git/safe` from the repo root.
---
+++ lua/trusted-local/init.lua
@@ -0,0 +1,20 @@
+return {
+    setup = function()
+        local au_group = vim.api.nvim_create_augroup("Trusted-local lua",
+                                                     {clear = true})
+
+        vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+            desc = "Trusted-local for lua",
+            pattern = "*",
+            callback = function()
+                print(debug.getinfo(1).currentline)
+                local trusted_local = vim.fn.getcwd() ..
+                                          "/.git/safe/../../.init.local.lua"
+                if vim.fn.filereadable(trusted_local) == 1 then
+                    vim.cmd("luafile " .. trusted_local)
+                end
+            end,
+            group = au_group
+        })
+    end
+}
